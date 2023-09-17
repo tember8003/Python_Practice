39b4fff8e876e9a9c26bfb1d97857eba99925a7b
tember8003 <104021663+tember8003@users.noreply.github.com>
2023-09-17 23:06:13 +0900

    Update main.py

--- Mile_to_Kilometer-27day/main.py
+++ Mile_to_Kilometer-27day/main.py
@@ -7,10 +7,11 @@ def mile_to_km():
 
 window=Tk()
 window.title("마일을 킬로미터로 바꾸기")
-window.config(padx=20,pady=20)
+window.config(padx=20,pady=20) #창 사이즈 조절.
 
 miles_input=Entry(width=7)
-miles_input.grid(column=1,row=0)
+miles_input.grid(column=1,row=0) #grid로 각각의 변수 위치를 조정함.
+
 miles_label=Label(text="Miles")
 miles_label.grid(column=2,row=0)
 
@@ -23,7 +24,7 @@ kilometer_result_label.grid(column=1,row=1)
 kilometer_label = Label(text="Km")
 kilometer_label.grid(column=2,row=1)
 
-calculate_button=Button(text="Calculate",command=mile_to_km)
+calculate_button=Button(text="Calculate",command=mile_to_km) #command에 함수를 연결해서 버튼 누를 때 함수가 실행되도록 함.
 calculate_button.grid(column=1,row=2)
 
 window.mainloop()
